da905acfcf5ebb62ba410b7b3ca0b4838c0857a5
Mushroomator <ThomasPilz@gmx.de>
2022-06-01 15:26:34 +0200

    Test plugin unloading

--- pkg/bot/Bot_Pluggable.go
+++ pkg/bot/Bot_Pluggable.go
@@ -142,11 +142,12 @@ func (state *Bot) handleLoadPlugin(ctx actor.Context, message *msg.LoadPlugin) {
 // Handle *msg.UnloadPlugin message
 func (state *Bot) handleUnloadPlugin(ctx actor.Context, message *msg.UnloadPlugin) {
 	// check if plugin is already loaded
-	pluginIdent := plgn.NewPluginIdentifier(message.Plugin.Name, message.Plugin.Version)
+	var pluginIdent plgn.Plugin = plgn.NewPluginIdentifier(message.Plugin.Name, message.Plugin.Version)
 	logger.Info("plugin found in active plugins?", log.Bool("ok", state.activePlugins.Contains(pluginIdent)), log.Stringer("plugin", pluginIdent))
 	if state.activePlugins.Contains(pluginIdent) {
 		// plugin must exist in here now, so we can ignore ok parameter
 		toBeDeactivated, ok := state.loadedPlugins[pluginIdent]
+		logger.Info("loaded plugins: ", log.Object("loadedPlugins", state.loadedPlugins))
 		logger.Info("plugin found in loaded plugins?", log.Bool("ok", ok))
 		if ok {
 			logger.Info("plugin", log.Object("pluginMethods", toBeDeactivated))
